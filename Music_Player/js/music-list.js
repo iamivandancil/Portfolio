f80094132a9674ecbf3fd58b6af4c12b2de5b532
Ivan Dancil <ivanbdancil@gmail.com>
2022-06-04 10:47:46 +0800

    first commit in portfolio

---
+++ Music_Player/js/music-list.js
@@ -0,0 +1,100 @@
+let allMusic = [
+    {
+        name: "Because of You",
+        artist: "Ne-Yo",
+        img: "image_1",
+        src: "music_1"
+    },
+    {
+        name: "Wet the Bed",
+        artist: "Chris Brown",
+        img: "image_2",
+        src: "music_2"
+    },
+    {
+        name: "Swalla",
+        artist: "Jason Derulo",
+        img: "image_3",
+        src: "music_3"
+    },
+    {
+        name: "Mad",
+        artist: "Ne-Yo",
+        img: "image_4",
+        src: "music_4"
+    },
+    {
+        name: "Miss Independent",
+        artist: "Ne-Yo",
+        img: "image_5",
+        src: "music_5"
+    },
+    {
+        name: "Booty Wurk",
+        artist: "T-pain",
+        img: "image_6",
+        src: "music_6"
+    },
+    {
+        name: "Together",
+        artist: "Ne-Yo",
+        img: "image_1",
+        src: "music_7"
+    },
+    {
+        name: "Next to Your",
+        artist: "Chris Brown",
+        img: "image_2",
+        src: "music_8"
+    },
+    {
+        name: "Beautiful Girls",
+        artist: "Sean Kingston",
+        img: "image_3",
+        src: "music_9"
+    },
+    {
+        name: "Payphone",
+        artist: "Maroon 5 ft. Wiz Khalifa",
+        img: "image_4",
+        src: "music_10"
+    },
+    {
+        name: "Fire Burning",
+        artist: "Sean Kingston",
+        img: "image_5",
+        src: "music_11"
+    },
+    {
+        name: "Me Lovee",
+        artist: "Sean Kingston",
+        img: "image_6",
+        src: "music_12"
+    },
+    {
+        name: "So Sick",
+        artist: "Ne-Yo",
+        img: "image_4",
+        src: "music_13"
+    },
+    {
+        name: "Titig",
+        artist: "Audio",
+        img: "image_10",
+        src: "music_15"
+    },
+    {
+        name: "Bahala Ka",
+        artist: "Audio",
+        img: "image_8",
+        src: "music_16"
+    },
+    {
+        name: "Wag na lang",
+        artist: "Audio",
+        img: "image_12",
+        src: "music_17"
+    },
+    
+ 
+]
